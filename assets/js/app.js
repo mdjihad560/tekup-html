407e60de35f00cad0c5b1106840ed7544d028046
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2024-03-26 16:31:20 +0600

    live verion

---
+++ assets/js/app.js
@@ -0,0 +1,313 @@
+"use strict";
+
+(function ($) {
+  "use strict";
+
+  /*------------------------------------------------------------------
+  [Table of contents]
+  tekup PRELOADER JS INIT
+  tekup SKILLBAR JS INIT
+  tekup MENU SIDEBAR JS INIT
+  tekup COUNTER JS INIT
+  tekup IMAGE ROTATE JS INIT
+  tekup AUTO SLIDER JS INIT
+  tekup PROJECT SLIDER JS INIT
+  tekup PROJECT SLIDER2 JS INIT
+  tekup MAGNIFIC POPUP JS INIT
+  tekup IMAGE SWIPE HOVER JS INIT
+  tekup PRICING TABLE JS INIT
+  tekup MAP JS
+  tekup wow js
+  
+  -------------------------------------------------------------------*/
+
+  /*--------------------------------------------------------------
+  CUSTOM PRE DEFINE FUNCTION
+  ------------------------------------------------------------*/
+  /* is_exist() */
+  jQuery.fn.is_exist = function () {
+    return this.length;
+  };
+  $(function () {
+    /*--------------------------------------------------------------
+    tekup PRELOADER JS INIT
+    --------------------------------------------------------------*/
+
+    $(".tekup-preloader-wrap").fadeOut(500);
+
+    /*--------------------------------------------------------------
+    HEADER SEARCH JS INIT
+    ------------------------------------------------------------*/
+    $(".tekup-header-search, .tekup-header-search-close").click(function () {
+      $(".tekup-header-search-section").toggleClass("open");
+    });
+
+    /*--------------------------------------------------------------
+    tekup STICKY MENU JS INIT
+    --------------------------------------------------------------*/
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop() > 50) {
+        $('#sticky-menu').addClass('sticky-menu');
+      } else {
+        $('#sticky-menu').removeClass('sticky-menu');
+      }
+    });
+
+    /*--------------------------------------------------------------
+    tekup MENU SIDEBAR JS INIT
+    --------------------------------------------------------------*/
+    $(".tekup-header-barger").on("click", function (e) {
+      $(".tekup-sidemenu-column, .offcanvas-overlay").addClass("active");
+      event.preventDefault(e);
+    });
+    $(".tekup-sidemenu-close, .offcanvas-overlay").on("click", function () {
+      $(".tekup-sidemenu-column, .offcanvas-overlay").removeClass("active");
+    });
+
+    /*--------------------------------------------------------------
+    ZUBUZ BRAND SLIDER ONE
+    --------------------------------------------------------------*/
+    var hero_slider = $('.tekup-hero-slider');
+    if (hero_slider.is_exist()) {
+      hero_slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: false,
+        speed: 800,
+        lazyLoad: 'progressive',
+        prevArrow: '<button class="slide-arrow tekup-hero-next"></button>',
+        nextArrow: '<button class="slide-arrow tekup-hero-prev"></button>'
+      }).slickAnimation();
+    }
+
+    /*--------------------------------------------------------------
+    ZUBUZ BRAND SLIDER ONE
+    --------------------------------------------------------------*/
+    // var zubuz_brand_slider = $('.zubuz-brand-slider');
+    // if(zubuz_brand_slider.is_exist()){
+    //   zubuz_brand_slider.slick({
+    //   infinite: true,
+    //   slidesToShow: 3, 
+    //   slidesToScroll: 1,
+    //   arrows: false,
+    //   dots: false,
+    //   autoplay: true , 
+    //   autoplaySpeed: 0,
+    //   speed: 10000,
+    //   cssEase: 'linear',
+    //   pauseOnHover:true,
+    //   adaptiveHeight: true,
+    //   responsive: [
+    //   {
+    //     breakpoint: 991,
+    //     settings: {
+    //       slidesToShow: 3,
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 767,
+    //     settings: {
+    //       slidesToShow: 2,
+    //     }
+    //   },
+
+    //   ]
+    // });
+
+    // }
+
+    /*--------------------------------------------------------------
+    tekup COUNTER JS INIT
+    --------------------------------------------------------------*/
+    var tekup_counter = $('#tekup-counter');
+    if (tekup_counter.is_exist()) {
+      var a = 0;
+      $(window).scroll(function () {
+        var oTop = $(tekup_counter).offset().top - window.innerHeight;
+        if (a == 0 && $(window).scrollTop() > oTop) {
+          $('.tekup-counter').each(function () {
+            var $this = $(this),
+              countTo = $this.attr('data-percentage');
+            $({
+              countNum: $this.text()
+            }).animate({
+              countNum: countTo
+            }, {
+              duration: 4000,
+              easing: 'swing',
+              step: function step() {
+                $this.text(Math.floor(this.countNum));
+              },
+              complete: function complete() {
+                $this.text(this.countNum);
+              }
+            });
+          });
+          a = 1;
+        }
+      });
+    }
+
+    /*--------------------------------------------------------------
+    tekup MAGNIFIC POPUP JS INIT
+    ------------------------------------------------------------*/
+    var popup_youtube = $('.video-init');
+    if (popup_youtube.is_exist()) {
+      popup_youtube.magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade'
+      });
+    }
+  }); /*End document ready*/
+
+  $(window).on("resize", function () {}); // end window resize
+
+  $(window).on("load", function () {}); // End window LODE
+
+  /*--------------------------------------------------------------
+  tekup MAP JS
+  ------------------------------------------------------------*/
+  var google_map = $('#map');
+  if (google_map.is_exist()) {
+    var init = function init() {
+      var mapOptions = {
+        zoom: 11,
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: true,
+        disableDefaultUI: true,
+        center: new google.maps.LatLng(40.6700, -73.9400),
+        styles: [{
+          "featureType": "landscape.man_made",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#f7f1df"
+          }]
+        }, {
+          "featureType": "landscape.natural",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#d0e3b4"
+          }]
+        }, {
+          "featureType": "landscape.natural.terrain",
+          "elementType": "geometry",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "poi",
+          "elementType": "labels",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "poi.business",
+          "elementType": "all",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "poi.medical",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#fbd3da"
+          }]
+        }, {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#bde6ab"
+          }]
+        }, {
+          "featureType": "road",
+          "elementType": "geometry.stroke",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "road",
+          "elementType": "labels",
+          "stylers": [{
+            "visibility": "off"
+          }]
+        }, {
+          "featureType": "road.highway",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "color": "#ffe15f"
+          }]
+        }, {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [{
+            "color": "#efd151"
+          }]
+        }, {
+          "featureType": "road.arterial",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "color": "#ffffff"
+          }]
+        }, {
+          "featureType": "road.local",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "color": "black"
+          }]
+        }, {
+          "featureType": "transit.station.airport",
+          "elementType": "geometry.fill",
+          "stylers": [{
+            "color": "#cfb2db"
+          }]
+        }, {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [{
+            "color": "#a2daf2"
+          }]
+        }]
+      };
+      var mapElement = document.getElementById('map');
+      var map = new google.maps.Map(mapElement, mapOptions);
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(40.6700, -73.9400),
+        map: map,
+        // icon: 'assets/images/all-img/contact/map.png',
+        title: 'fugu'
+      });
+      var contentString = '<div id="content">' + '<div id="tpw">' + '<h3>fugu' + '</div>';
+      var infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 280
+      });
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function () {
+        marker.setAnimation(null);
+      }, 750); //time it takes for one bounce   
+
+      google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+      });
+    };
+    google.maps.event.addDomListener(window, 'load', init);
+  }
+
+  //tekup wow js
+  var wow = new WOW({
+    mobile: false,
+    // default
+    tablet: false
+  });
+  wow.init();
+
+  // AOS.init({
+  //   duration: 1200,
+  // })
+})(jQuery);
